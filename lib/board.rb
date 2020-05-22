626e3568633e206cc80b1723ab459009d3df2e2b
compiled-convention-1791 <melody.bebee@gmail.com>
2020-05-22 05:36:52 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,1 +1,25 @@
+row1 = [" "," "," ",]
+
+row1[0]
+row1[1]
+row1[2]
+
+row1[1]="X"
+
+row2 = [" "," "," ",]
+
+row2[0]
+row2[1]
+row2[2]
+
+row2[1]="X"
+
+row3 = [" "," "," ",]
+
+row3[0]
+row3[1]
+row3[2]
+
+row3[1]="X"
+
 # Define the variable board below.
