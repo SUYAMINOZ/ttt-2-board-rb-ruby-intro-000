9175205504fa66398ae92154310ec2bbc66b65f5
compiled-convention-1791 <melody.bebee@gmail.com>
2020-05-22 05:45:54 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,25 +1,4 @@
-row1 = [" "," "," ",]
-
-row1[0]
-row1[1]
-row1[2]
-
-row1[1]="X"
-
-row2 = [" "," "," ",]
-
-row2[0]
-row2[1]
-row2[2]
-
-row2[1]="X"
-
-row3 = [" "," "," ",]
-
-row3[0]
-row3[1]
-row3[2]
-
-row3[1]="X"
+board = [" ","X"," "," ","X"," ","X"," "
+]
 
 # Define the variable board below.
